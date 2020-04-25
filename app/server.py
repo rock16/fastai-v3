5c3c71ee3d82e6dfd8c5803cf8e5c8787c31cd2a
rock okechukwu <rockok10@gmail.com>
2020-04-25 18:56:41 +0100

    customize app for my color model

--- app/server.py
+++ app/server.py
@@ -9,10 +9,10 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
+export_file_url = 'https://www.googleapis.com/drive/v3/files/1-1JkT6je6z8AHdKHw-XBfvlkrYIR02Qy?alt=media&key=AIzaSyBdCzFtheRIM3imBc4p1unWPA-D3Og1GgY'
 export_file_name = 'export.pkl'
 
-classes = ['black', 'grizzly', 'teddys']
+classes = ['red', 'green', 'blue', 'grey']
 path = Path(__file__).parent
 
 app = Starlette()
